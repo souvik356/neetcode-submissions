fe2122bf3a18b60dabdfec6873f1acf27c9f782e
Souvik Roy <72182566+souvik356@users.noreply.github.com>
2026-06-20 15:54:05 +0530

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length !== t.length) return false
+        let map={}
+        for(let i=0;i<s.length;i++){
+            if(!map[s[i]]){
+                map[s[i]] = 1
+            }else{
+               ++map[s[i]]
+            }
+        }
+        console.log(map)
+
+        for(let i=0;i<t.length;i++){
+            if(!map[t[i]]){
+                return false
+            }else{
+                --map[t[i]]
+            }
+        }
+        return true
+    }
+}
