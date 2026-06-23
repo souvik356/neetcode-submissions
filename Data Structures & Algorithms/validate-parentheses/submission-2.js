acb91d422a939c574c979d86adc6e06874c009fb
Souvik Roy <72182566+souvik356@users.noreply.github.com>
2026-06-23 20:04:37 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        let stack=[]
+        for(let i=0;i<s.length;i++){
+            let ch = s[i]
+            if(ch == "[" || ch == "(" || ch == "{"){
+                stack.push(ch)
+            }else{
+                if(stack.length == 0) return false
+                let peek = stack[stack.length-1]
+                if((peek == "(" && ch == ")") || (peek=="[" && ch=="]") || (peek=="{" && ch=="}")){
+                    stack.pop()
+                }
+                else{
+                    return false
+                }
+            }
+        }
+        // console.log(stack)
+        if(stack.length == 0){
+            return true
+        }else{
+            return false
+        }
+    }
+}
